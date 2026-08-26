930bbfe66398bda8dda74e8655b3ce542fc42bc0
402-02-kritsanapat <54149@yupparaj.ac.th>
2026-08-26 15:30:57 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -100,10 +100,10 @@ st.session_state.ans2_val = ans2
 
 # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 st.session_state.ans2_val = ans3
-    "ข้อ 3: An `Ba_a_a` a day keeps the doctor away. 🍌",
+    "ข้อ 3: I love to eat `Ba_a_a` . 🍌",
     value=st.session_state.ans3_val,
 st.session_state.ans2_val = ans4
-    "ข้อ 4: An `L_mo_` a day keeps the doctor away. 🍋",
+    "ข้อ 4: I love to eat `L_mo_` . 🍋",
     value=st.session_state.ans4_val,
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
